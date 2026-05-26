ede815a2666c593c044ed84cb0d93d2bf57958dc
Prathamesh R Bhumkar <p.r.bhumkar13@gmail.com>
2026-05-26 11:28:41 +0530

    change made reslove error in app.get :wq

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express();
 const PORT = 5000;
 
 // A basic route to test if the server works
-app.get('/', (req, res) => {
+
  app.get('/', (req, res) => {
     // Both lines sent together in ONE response
     res.send('Hello, your backend server is officially alive and running!<br>I am Brandi Love');
-});
 });
 
 // Start listening for web requests
