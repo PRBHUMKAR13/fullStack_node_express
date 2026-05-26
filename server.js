9f34c578a56e44aa4f6f33ea122cc04acf2765f1
Prathamesh R Bhumkar <p.r.bhumkar13@gmail.com>
2026-05-26 11:12:06 +0530

    feat:intialize express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// A basic route to test if the server works
+app.get('/', (req, res) => {
+ app.get('/', (req, res) => {
+    // Both lines sent together in ONE response
+    res.send('Hello, your backend server is officially alive and running!<br>I am Brandi Love');
+});
+});
+
+// Start listening for web requests
+app.listen(PORT, () => {
+    console.log(`🚀 Server is listening dynamically on- http://localhost:${PORT}`);
+});
